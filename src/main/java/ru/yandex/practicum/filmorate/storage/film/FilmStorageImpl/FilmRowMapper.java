b602194b1d6811f2c9198d47cf51b2f1524f166c
mat973 <matvey.oleynik.2017@mail.ru>
2025-04-14 21:04:36 +0300

    finale try(3)

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorageImpl/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorageImpl/FilmRowMapper.java
@@ -11,9 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class FilmRowMapper implements RowMapper<Film> {
